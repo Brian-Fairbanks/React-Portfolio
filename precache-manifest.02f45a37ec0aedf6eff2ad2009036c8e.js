b24404c3c3223d7fcb03cb96ada6299bf101b59a
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-20 22:24:12 -0500

    Updates

---
+++ precache-manifest.02f45a37ec0aedf6eff2ad2009036c8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c17a6e212eebf709af5131b7eeb002",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "97ca644804a6b04b376f",
+    "url": "/react-portfolio/static/css/main.49ca4100.chunk.css"
+  },
+  {
+    "revision": "bd5a97b585625935462b",
+    "url": "/react-portfolio/static/js/2.b50f9d4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.b50f9d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ca644804a6b04b376f",
+    "url": "/react-portfolio/static/js/main.aead24b3.chunk.js"
+  },
+  {
+    "revision": "bc85d7a008d996148277",
+    "url": "/react-portfolio/static/js/runtime-main.8b0e39e5.js"
+  },
+  {
+    "revision": "d37bafab271b75831ea9dcbfdc8db517",
+    "url": "/react-portfolio/static/media/Brian_Fairbanks_resume_04082020.d37bafab.pdf"
+  },
+  {
+    "revision": "0aa32de8d2483ecbb1c93a029e877fcd",
+    "url": "/react-portfolio/static/media/backtile.0aa32de8.jpg"
+  },
+  {
+    "revision": "7564f0412d4979dc23d08e699cfaa9de",
+    "url": "/react-portfolio/static/media/self-s.7564f041.png"
+  },
+  {
+    "revision": "bb4671611c2a044a34e8a45d4e12e63c",
+    "url": "/react-portfolio/static/media/self.bb467161.png"
+  }
+]);
