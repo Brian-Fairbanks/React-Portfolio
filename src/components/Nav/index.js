c4638a3bde76f134d5356b8587496136f76492e2
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-21 10:32:39 -0500

    Corrected links in nav to corrospond to new routes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,13 +6,13 @@ function Nav(props) {
     <nav className="navbar navbar-expand-md col-md-7 justify-content-center justify-content-md-end">
       <ul className="nav">
         <li className="nav-item">
-          <Link to={"/"} className="nav-link active">About</Link>
+          <Link to={"/React-Portfolio/"} className="nav-link active">About</Link>
         </li>
         <li className="nav-item">
-          <Link to={"/portfolio"} className="nav-link active">Portfolio</Link>
+          <Link to={"/React-Portfolio/portfolio"} className="nav-link active">Portfolio</Link>
         </li>
         <li className="nav-item">
-          <Link to={"/contact"} className="nav-link active">Contact</Link>
+          <Link to={"/React-Portfolio/contact"} className="nav-link active">Contact</Link>
         </li>
       </ul>
     </nav>
